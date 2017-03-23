b470bf29999165cde58e6f49e74cd3f268e2bec4
Sagar Singh <sagar.deora@qed42.com>
2017-03-23 12:31:41 +0530

    Initial Commit

---
+++ themes/internship2017/gulpfile.js
@@ -0,0 +1,51 @@
+var gulp = require('gulp');
+var jshint = require('gulp-jshint');
+var imagemin = require('gulp-imagemin');
+var compass = require('gulp-compass');
+var browserSync = require('browser-sync').create();
+
+gulp.task('compass', function() {
+  gulp.src('sass/**/*.scss')
+    .pipe(compass({
+      config_file: 'config.rb',
+      css: 'css',
+      sass: 'sass'
+    }))
+    .pipe(gulp.dest('css/'));
+});
+
+gulp.task('browserSync',function(){
+  browserSync.init({
+    proxy:"drupal.theme/"
+  })
+})
+
+gulp.task('jshint', function () {
+    return gulp.src('js/*.js')
+            .pipe(jshint())
+            .pipe(jshint.reporter('jshint-stylish'));
+});
+
+gulp.task('imagemin', () =>
+    gulp.src('images/*')
+        .pipe(imagemin())
+        .pipe(gulp.dest('images'))
+);
+
+gulp.task('styles',function(){
+gulp.src(['css/main.css'])
+.pipe(browserSync.reload({
+  stream:true
+}))
+})
+// gulp.task('default',['compass','imagemin','jshint',]),function(){
+// gulp.watch("saas/**/*.scss",['compass']);
+// gulp.watch("js/**/*.js",['jshint']);
+// }
+
+gulp.task('default', ['compass', 'browserSync'], function() {
+   gulp.watch("sass/**/*.scss", ['compass']);
+  gulp.watch("css/*.css", ['styles']);
+   gulp.watch("drupal.theme/", ['browserSync']);
+});
+
