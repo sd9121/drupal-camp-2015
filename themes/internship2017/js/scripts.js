f3b1f35c5f3c766c2eca76d24dbcf8c8bdea8afa
Sagar Singh <sagar.deora@qed42.com>
2017-03-26 16:12:24 +0530

    Done responsive.

--- themes/internship2017/js/scripts.js
+++ themes/internship2017/js/scripts.js
@@ -1,1 +1,14 @@
-// alert('hello');
+ var navShowHide = function() {
+     var menu_div = document.getElementsByClassName("menu")[0];
+     var classes = menu_div.className;
+     if (classes.indexOf('mobileview-hide') == -1) {
+         menu_div.className += 'mobileview-hide';
+     } else {
+         menu_div.className = classes.replace('mobileview-hide','');
+     }
+     // var logoImage = document.getElementsByClassName("logo-image")[0];
+     // logoImage.style.display = 'none';
+ }
+
+ var menu_element = document.getElementsByClassName("menu-button");
+ menu_element[0].onclick = navShowHide;
