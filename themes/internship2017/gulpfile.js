6be984bc4f52a120ac4cd5412ea2cc268ce3570a
Sagar Singh <sagar.deora@qed42.com>
2017-03-27 15:20:59 +0530

    used autoprefixer .

--- themes/internship2017/gulpfile.js
+++ themes/internship2017/gulpfile.js
@@ -3,49 +3,53 @@ var jshint = require('gulp-jshint');
 var imagemin = require('gulp-imagemin');
 var compass = require('gulp-compass');
 var browserSync = require('browser-sync').create();
-
+var autoprefixer = require('gulp-autoprefixer');
 gulp.task('compass', function() {
-  gulp.src('sass/**/*.scss')
-    .pipe(compass({
-      config_file: 'config.rb',
-      css: 'css',
-      sass: 'sass'
-    }))
-    .pipe(gulp.dest('css/'));
+    gulp.src('sass/**/*.scss')
+        .pipe(compass({
+            config_file: 'config.rb',
+            css: 'css',
+            sass: 'sass'
+        }))
+        .pipe(gulp.dest('css/'));
 });
 
-gulp.task('browserSync',function(){
-  browserSync.init({
-    proxy:"drupal.theme/"
-  })
+gulp.task('autoprefixer', () =>
+    gulp.src('css/main.css')
+    .pipe(autoprefixer({
+        browsers: ['last 2 versions'],
+        cascade: false
+    }))
+    .pipe(gulp.dest('css/'))
+);
+
+gulp.task('browserSync', function() {
+    browserSync.init({
+        proxy: "drupal.theme/"
+    })
 })
 
-gulp.task('jshint', function () {
+gulp.task('jshint', function() {
     return gulp.src('js/*.js')
-            .pipe(jshint())
-            .pipe(jshint.reporter('jshint-stylish'));
+        .pipe(jshint())
+        .pipe(jshint.reporter('jshint-stylish'));
 });
 
 gulp.task('imagemin', () =>
     gulp.src('images/*')
-        .pipe(imagemin())
-        .pipe(gulp.dest('images'))
+    .pipe(imagemin())
+    .pipe(gulp.dest('images'))
 );
 
-gulp.task('styles',function(){
-gulp.src(['css/main.css'])
-.pipe(browserSync.reload({
-  stream:true
-}))
+gulp.task('styles', function() {
+    gulp.src(['css/main.css'])
+        .pipe(browserSync.reload({
+            stream: true
+        }))
 })
-// gulp.task('default',['compass','imagemin','jshint',]),function(){
-// gulp.watch("saas/**/*.scss",['compass']);
-// gulp.watch("js/**/*.js",['jshint']);
-// }
-
-gulp.task('default', ['compass', 'browserSync'], function() {
-   gulp.watch("sass/**/*.scss", ['compass']);
-  gulp.watch("css/*.css", ['styles']);
-   gulp.watch("drupal.theme/", ['browserSync']);
+gulp.task('default', ['compass', 'styles', 'browserSync', 'autoprefixer'], function() {
+    gulp.watch("sass/**/*.scss", ['compass']);
+    gulp.watch("css/*.css", ['styles']);
+    gulp.watch("drupal.theme/", ['browserSync']);
+    gulp.watch("css/*.css", ['autoprefixer']);
 });
-
